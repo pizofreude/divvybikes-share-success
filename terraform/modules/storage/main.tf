b8062800a46ae14d281e69e013c2c3bbe87823be
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2025-08-16 02:35:33 +0800

    feat: create glue resources

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -408,3 +408,281 @@ resource "aws_s3_bucket_policy" "gold_policy" {
     ]
   })
 }
+
+# =============================================================================
+# AWS Glue Data Catalog Resources
+# =============================================================================
+
+# Glue Database for Bronze layer external tables
+resource "aws_glue_catalog_database" "bronze_db" {
+  name = "${var.project_name}_bronze_db"
+  
+  description = "Data catalog database for Divvy Bikes Bronze layer external tables"
+
+  tags = merge(var.common_tags, {
+    Name        = "${var.project_name}-${var.environment}-bronze-db"
+    Layer       = "bronze"
+    Description = "Glue catalog database for external tables pointing to S3 bronze data"
+  })
+}
+
+# =============================================================================
+# Glue Catalog Tables for Bronze Layer Data
+# =============================================================================
+
+# Glue Catalog Table for Divvy Trips Data
+resource "aws_glue_catalog_table" "divvy_trips" {
+  name          = "divvy_trips"
+  database_name = aws_glue_catalog_database.bronze_db.name
+  description   = "Divvy bike share trip data partitioned by year and month"
+
+  table_type = "EXTERNAL_TABLE"
+
+  parameters = {
+    "skip.header.line.count" = "1"
+    "delimiter"              = ","
+    "classification"         = "csv"
+  }
+
+  storage_descriptor {
+    location      = "s3://${aws_s3_bucket.bronze.id}/divvy-trips/"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      serialization_library = "org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe"
+      parameters = {
+        "field.delim" = ","
+      }
+    }
+
+    columns {
+      name = "ride_id"
+      type = "string"
+    }
+    columns {
+      name = "rideable_type"
+      type = "string"
+    }
+    columns {
+      name = "started_at"
+      type = "timestamp"
+    }
+    columns {
+      name = "ended_at"
+      type = "timestamp"
+    }
+    columns {
+      name = "start_station_name"
+      type = "string"
+    }
+    columns {
+      name = "start_station_id"
+      type = "string"
+    }
+    columns {
+      name = "end_station_name"
+      type = "string"
+    }
+    columns {
+      name = "end_station_id"
+      type = "string"
+    }
+    columns {
+      name = "start_lat"
+      type = "double"
+    }
+    columns {
+      name = "start_lng"
+      type = "double"
+    }
+    columns {
+      name = "end_lat"
+      type = "double"
+    }
+    columns {
+      name = "end_lng"
+      type = "double"
+    }
+    columns {
+      name = "member_casual"
+      type = "string"
+    }
+  }
+
+  partition_keys {
+    name = "year"
+    type = "string"
+  }
+  partition_keys {
+    name = "month"
+    type = "string"
+  }
+}
+
+# Glue Catalog Table for Weather Data
+resource "aws_glue_catalog_table" "weather_data" {
+  name          = "weather_data"
+  database_name = aws_glue_catalog_database.bronze_db.name
+  description   = "Weather data partitioned by location, year, and month"
+
+  table_type = "EXTERNAL_TABLE"
+
+  parameters = {
+    "skip.header.line.count" = "1"
+    "delimiter"              = ","
+    "classification"         = "csv"
+  }
+
+  storage_descriptor {
+    location      = "s3://${aws_s3_bucket.bronze.id}/weather-data/"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      serialization_library = "org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe"
+      parameters = {
+        "field.delim" = ","
+      }
+    }
+
+    columns {
+      name = "time"
+      type = "date"
+    }
+    columns {
+      name = "temperature_2m_max"
+      type = "double"
+    }
+    columns {
+      name = "temperature_2m_min"
+      type = "double"
+    }
+    columns {
+      name = "temperature_2m_mean"
+      type = "double"
+    }
+    columns {
+      name = "apparent_temperature_max"
+      type = "double"
+    }
+    columns {
+      name = "apparent_temperature_min"
+      type = "double"
+    }
+    columns {
+      name = "apparent_temperature_mean"
+      type = "double"
+    }
+    columns {
+      name = "precipitation_sum"
+      type = "double"
+    }
+    columns {
+      name = "rain_sum"
+      type = "double"
+    }
+    columns {
+      name = "snowfall_sum"
+      type = "double"
+    }
+    columns {
+      name = "wind_speed_10m_max"
+      type = "double"
+    }
+    columns {
+      name = "wind_gusts_10m_max"
+      type = "double"
+    }
+    columns {
+      name = "wind_direction_10m_dominant"
+      type = "int"
+    }
+    columns {
+      name = "cloud_cover_mean"
+      type = "int"
+    }
+  }
+
+  partition_keys {
+    name = "location"
+    type = "string"
+  }
+  partition_keys {
+    name = "year"
+    type = "string"
+  }
+  partition_keys {
+    name = "month"
+    type = "string"
+  }
+}
+
+# Glue Catalog Table for GBFS Stations Data
+resource "aws_glue_catalog_table" "gbfs_stations" {
+  name          = "gbfs_stations"
+  database_name = aws_glue_catalog_database.bronze_db.name
+  description   = "GBFS station information partitioned by endpoint, year, month, and day"
+
+  table_type = "EXTERNAL_TABLE"
+
+  parameters = {
+    "classification" = "json"
+  }
+
+  storage_descriptor {
+    location      = "s3://${aws_s3_bucket.bronze.id}/gbfs-data/"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      serialization_library = "org.openx.data.jsonserde.JsonSerDe"
+    }
+
+    columns {
+      name = "station_id"
+      type = "string"
+    }
+    columns {
+      name = "name"
+      type = "string"
+    }
+    columns {
+      name = "short_name"
+      type = "string"
+    }
+    columns {
+      name = "lat"
+      type = "double"
+    }
+    columns {
+      name = "lon"
+      type = "double"
+    }
+    columns {
+      name = "capacity"
+      type = "int"
+    }
+    columns {
+      name = "legacy_id"
+      type = "string"
+    }
+  }
+
+  partition_keys {
+    name = "endpoint"
+    type = "string"
+  }
+  partition_keys {
+    name = "year"
+    type = "string"
+  }
+  partition_keys {
+    name = "month"
+    type = "string"
+  }
+  partition_keys {
+    name = "day"
+    type = "string"
+  }
+}
